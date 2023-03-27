75aa95aa35f3c30630dcc0781514010161f2d8c8
halilov-frondend <halilovabdulhafiz2005@gmail.com>
2023-03-27 23:26:34 +0500

    finished

---
+++ app.js
@@ -0,0 +1,27 @@
+// Algoritm 1
+
+// function summOfArray(arr) {
+//     let sum = 0;
+  
+//     for (let i = 0; i < arr.length; i++) {
+//       sum += arr[i];
+//     }
+  
+//     return sum;
+//   }
+  
+//   console.log(summOfArray([1, 2, 3, 4, 5]));
+
+// Algoritm 2
+// function generateAndSumm(num) {
+//   let sum = 0;
+
+//   for (let i = 0; i <= num; i++) {
+//     sum += i;
+//     console.log(sum);
+//   }
+
+//   return sum;
+// }
+
+// console.log(generateAndSumm(5));
